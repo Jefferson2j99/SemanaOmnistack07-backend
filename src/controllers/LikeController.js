dfb4b9b64cd681e10c7d9104558075b63c7ab95f
Nelio Alves <jefferson2112@hotmail.com>
2019-10-29 13:28:52 -0300

    -

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -4,11 +4,7 @@ module.exports = {
   async store(req, res) {
     const post = await Post.findById(req.params.id);
 
-    if (post.likes < 1) {
-      post.likes += 1;
-    } else {
-      post.likes -= 1;
-    }
+    post.likes += 1;
 
     await post.save();
 
